a787fbc7069b73cd20449cd9bd13e360f13482f5
unknown <abdelhakim@univ-tiaret.dz>
2020-10-10 13:27:26 +0100

    getimage 3

--- lib/views/create_blog.dart
+++ lib/views/create_blog.dart
@@ -67,7 +67,7 @@ class _CreateBlogState extends State<CreateBlog> {
           onTap: (){
             getImage();
           },
-          child: selectedImage == null ? Container()
+          child: selectedImage != null ? Container()
           : Container(
           height: 100, 
           decoration: BoxDecoration(color: Colors.white, borderRadius: BorderRadius.circular(15),border: Border.all(),),
